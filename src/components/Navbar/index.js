3d1b1f19401c1c36527e79e7b9f467741666981f
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-07 16:32:28 +0300

    add redux to navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { IoLogOutOutline } from "react-icons/io5";
 import { MdSpaceDashboard } from "react-icons/md";
 import { AiFillHome } from "react-icons/ai";
+import { userLogout } from "./../../reducers/Login";
 import "./style.css";
 
 const Navbar = ({ page }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const state = useSelector((state) => {
+    return {
+      role: state.Login.role,
+    };
+  });
+
 
   const logout = () => {
-    //
+    dispatch(userLogout({ role: "", token: "" }));
     navigate("/login");
   };
 
-  return role ? (
+  return state.role ? (
     <div className="navbar">
       {page !== "Todos" && (
         <AiFillHome className="homeLogo" onClick={() => navigate("/")} />
       )}
-      {page === "Todos" && role === "admin" && (
+      {page === "Todos" && state.role === "admin" && (
         <MdSpaceDashboard
           className="dashboardLogo"
           onClick={() => navigate("/dashboard")}
